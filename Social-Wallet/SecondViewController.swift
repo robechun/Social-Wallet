8f52f135cc378d9c84e175123dfd1e10c92349e9
Robert Chung <robertchung97@gmail.com>
2018-10-09 21:45:25 -0700

    Xcode initialization

---
+++ Social-Wallet/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Social-Wallet
+//
+//  Created by Robert Chung on 10/9/18.
+//  Copyright © 2018 Robert Chung. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
